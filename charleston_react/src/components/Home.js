f3fc62c82c7585991848523be89a13579eed5f84
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2018-01-02 22:01:38 +0100

    Working on home page

--- charleston_react/src/components/Home.js
+++ charleston_react/src/components/Home.js
@@ -7,38 +7,59 @@ class Home extends Component {
 	render() {
 		return (
 			<div>
-			<div id="intro">
-				<h2> ~ Embark with us on a magical journey into charleston ~ </h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-				ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-				laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<p><img src={gif} alt="loading..." id="gif" /></p>
-			</div>
-
-			<div id="history">
-				<h3>History of charleston</h3>
+				<div id="intro">
+					<h2> ~ Embark with us on a magical journey into charleston ~ </h2>
 					<div className="row">
-						<div className="col-xs-6 col-xs-offset-1">
-							<p className="text-justify">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-							totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt 
-							explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur 
-							gni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor 
-							sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore 
-							magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
-							laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate 
-							velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+						<p className="col-xs-10 col-xs-offset-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+						ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+						laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</div>
+					<p><img src={gif} alt="loading..." id="gif" /></p>
+				</div>
+
+				<div id="history">
+					<h3>History of charleston</h3>
+						<div className="row">
+							<div className="col-xs-6 col-xs-offset-1">
+								<p className="text-justify">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+								totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt 
+								explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur 
+								gni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor 
+								sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore 
+								magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
+								laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate 
+								velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+							</div>
+							<div className="col-xs-4">
+								<img src={img1} alt="Josephine Baker dances Charleston" />
+							</div>
 						</div>
-						<div className="col-xs-5">
-							<img src={img1} alt="Josephine Baker dances Charleston" />
+				</div>
+
+				<div id="lastest_publications">
+					<h3>Last steps published</h3>
+					<div className="row">
+						<div className="col-xs-3 card">
+							<img src={img1} className="poster"/>
+							<h3>Step 1</h3>
+							<hr/>
+							<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+							<button>Read More</button>
+						</div>
+												<div className="col-xs-3 card">
+							<img src={img1} className="poster"/>
+							<h3>Step 1</h3>
+							<hr/>
+							<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+							<button>Read More</button>
 						</div>
 					</div>
-			</div>
-
-			<div id="lastest_publications">
-				<h3>Last steps published</h3>
 
 
-			</div>
+				</div>
+
+
+			
 			</div>
 
 
